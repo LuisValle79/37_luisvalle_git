94b0a51fc48197bda2a85b206088d565035beb02
LuisAlberto <141541553+LuisValle79@users.noreply.github.com>
2023-10-06 01:56:12 -0500

    java

---
+++ 37_luisvaalle/src/S06_37/Main_S06_37.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S06_37;
+
+/**
+ *
+ * @author Luis
+ */
+public class Main_S06_37 {
+         
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+    }
+
+}
